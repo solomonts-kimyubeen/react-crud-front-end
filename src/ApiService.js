8163c6a3794e86ff20fc75c5aeccd063544614c6
lwoblue <lwo_blue@naver.com>
2021-01-08 14:28:03 +0900

    react-crud-front-end

--- src/ApiService.js
+++ src/ApiService.js
@@ -2,8 +2,8 @@ import axios from "axios"
 
 
 // const USER_API_BASE_URL = "http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline"
-const USER_API_BASE_URL = "http://192.168.0.41:8080/users"
-// const USER_API_BASE_URL = "http://localhost:8080/users"
+// const USER_API_BASE_URL = "http://192.168.0.41:8080/users"
+const USER_API_BASE_URL = "http://localhost:8080/users"
 
 class ApiService {
 
